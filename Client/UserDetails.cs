ec01076e983eedf733c12287701b0f67dbb97177
Nayruden <nayruden@gmail.com>
2010-02-17 13:37:43 -0600

    Chugging away on the protocol implementation

---
+++ Client/UserDetails.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetTunnel
+{
+    // PORT: Don't both storing this, just store the last packet...
+    class UserDetails
+    {
+        public static UserDetails local_user = new UserDetails();
+        private static List<UserDetails> users = new List<UserDetails>();
+
+        public string nick;
+        public ulong userid = 0;
+        public List<Service> services = new List<Service>();
+
+        public UserMessage toUserMessage( MessageState state = MessageState.Modified )
+        {
+            var user_message = new UserMessage(nick, userid);
+            user_message.state = state;
+            user_message.services = services;
+
+            return user_message;
+        }
+
+        public static UserDetails getByUserid(ulong userid)
+        {
+            if (local_user.userid == userid)
+                return local_user;
+
+            return users.SingleOrDefault(u => u.userid == userid);
+        }
+
+        public static void add( UserDetails user )
+        {
+            users.Add(user);
+        }
+
+        public static void remove(ulong userid)
+        {
+            users.Remove(getByUserid(userid));
+        }
+    }
+}
---
+++ Shared/Helpers.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using System;
+using System.Runtime.Serialization;
+using System.IO;
+
+public static class StringExtensions
+{
+    public static string F(this string s, object arg0)
+    {
+        return string.Format(s, arg0);
+    }
+    public static string F(this string s, object arg0, object arg1)
+    {
+        return string.Format(s, arg0, arg1);
+    }
+    public static string F(this string s, object arg0, object arg1, object arg2)
+    {
+        return string.Format(s, arg0, arg1, arg2);
+    }
+    public static string F(this string s, params object[] args)
+    {
+        return string.Format(s, args);
+    }
+}
+
+namespace NetTunnel
+{
+    public class DebugMessage
+    {
+        [Conditional("DEBUG")]
+        public static void msg(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+
+    public class Utilities
+    {
+        private static DataContractSerializer ds = new DataContractSerializer(typeof(Message));
+
+        public static Message readMessage( byte[] buffer, int buffer_length )
+        {            
+            Message m;
+            using (var memory_stream = new MemoryStream(buffer, 0, buffer_length))
+                m = (Message)ds.ReadObject(memory_stream);
+
+            return m;
+        }
+
+        public static byte[] writeMessage(Message message, out int length)
+        {
+            var memory_stream = new MemoryStream();
+            ds.WriteObject(memory_stream, message);
+
+            length = (int)memory_stream.Length;
+            return memory_stream.GetBuffer();
+        }
+    }
+}
